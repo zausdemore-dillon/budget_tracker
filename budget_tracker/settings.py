38a257e10856231d319435d5741decf74c4447cc
Zac Ausdemore-Dillon <zausdemore-dillon@unomaha.edu>
2025-10-24 03:03:05 +0000

    Initial commit from EC2

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3-ayk%p&ut39v*0_dha*p3uv$wppjoj@0v@pm6(e^t!bgya@l#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.58.216.202']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
